14334b7daac8f6a06f926263b86dbdb4b11fee19
One <43485962+c-git@users.noreply.github.com>
2024-02-07 10:10:15 -0500

    Day 4 Part 2

--- src/day04.rs
+++ src/day04.rs
@@ -1,4 +1,7 @@
+use std::cmp::Ordering;
+
 use actix_web::{web, HttpResponse, Scope};
+use tracing::info;
 pub(crate) fn scope() -> Scope {
     web::scope("/4")
         .route("/strength", web::post().to(task1_reindeer_cheer))
@@ -9,13 +12,25 @@ pub(crate) fn scope() -> Scope {
 }
 
 #[derive(serde::Deserialize, Debug)]
-struct Reindeer {
-    name: String,
+struct Reindeer1 {
     strength: u32,
 }
 
+#[derive(serde::Deserialize, Debug)]
+struct Reindeer2 {
+    name: String,
+    strength: u32,
+    speed: f64,
+    height: u32,
+    antler_width: u32,
+    snow_magic_power: u32,
+    favorite_food: String,
+    #[serde(rename = "cAnD13s_3ATeN-yesT3rdAy")]
+    candies_eaten_yesterday: u32,
+}
+
 #[tracing::instrument]
-async fn task1_reindeer_cheer(reindeers: web::Json<Vec<Reindeer>>) -> String {
+async fn task1_reindeer_cheer(reindeers: web::Json<Vec<Reindeer1>>) -> String {
     reindeers
         .iter()
         .map(|x| x.strength)
@@ -24,6 +39,29 @@ async fn task1_reindeer_cheer(reindeers: web::Json<Vec<Reindeer>>) -> String {
 }
 
 #[tracing::instrument]
-async fn task2_cursed_candy_eating_contest() -> HttpResponse {
-    todo!()
+async fn task2_cursed_candy_eating_contest(reindeers: web::Json<Vec<Reindeer2>>) -> HttpResponse {
+    info!("start");
+    let fastest = reindeers
+        .iter()
+        .max_by(|x, y| {
+            if x.speed < y.speed {
+                Ordering::Less
+            } else {
+                Ordering::Greater
+            }
+        })
+        .unwrap();
+    let tallest = reindeers.iter().max_by_key(|x| x.height).unwrap();
+    let magician = reindeers.iter().max_by_key(|x| x.snow_magic_power).unwrap();
+    let consumer = reindeers
+        .iter()
+        .max_by_key(|x| x.candies_eaten_yesterday)
+        .unwrap();
+    HttpResponse::Ok().json(serde_json::json!({
+        "fastest": format!("Speeding past the finish line with a strength of {} is {}", fastest.strength, fastest.name),
+        "tallest": format!("{} is standing tall with his {} cm wide antlers", tallest.name, tallest.antler_width),
+        "magician": format!("{} could blast you away with a snow magic power of {}", magician.name, magician.snow_magic_power),
+        "consumer": format!("{} ate lots of candies, but also some {}", consumer.name, consumer.favorite_food),
+    }
+        ))
 }
